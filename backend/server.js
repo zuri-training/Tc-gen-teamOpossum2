f55d991ed06345ff259079bb6d736dfe56ed09e4
Samuel Olakada <olakadasamuel@gmail.com>
2022-12-06 15:10:53 +0100

    chore: updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 require('dotenv').config()
-const mongoose = require('mongoose')
 
 const userRoutes = require('./routes/userRoutes')
+const connectDB = require('./config/db')
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
-// middleware
+// connect to database
+connectDB()
+
+// middlewares
+app.use(express.json())
+// logger middleware
+app.use((req, res, next) => {
+    console.log(req.method, 'request made')
+    console.log(req.path)
+    next();
+})
+
+// User Route
 app.use('/user', userRoutes)
 
 app.listen(PORT, () => {
