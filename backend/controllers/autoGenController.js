185ce453a16bc5c184a11d604ee6a5276c65fa9d
Dev-wonderful <adeonederful20@gmail.com>
2022-12-12 06:21:27 +0100

    regex to input user details into the template file

--- backend/controllers/autoGenController.js
+++ backend/controllers/autoGenController.js
@@ -1,4 +1,6 @@
 const users = require('../model/User')
+const fsPromises = require('fs').promises
+const path = require('path')
 
 
 //verification middleware
@@ -13,6 +15,28 @@ async function userVerification (req, res, next) {
 }
 
 //File modification to generate T&C and Privacy Policy for users
-//...incoming
+//regex solution 1
+String.prototype.inputUserDetails = function(obj) {
+    var retStr = this;
+    for (var x in obj) {
+        retStr = retStr.replace(new RegExp(x, 'gi'), obj[x]);
+    }
+    return retStr;
+};
+
+//console.log('[NUMBER] is [DATE]'.allReplace({'\\[DATE]': 'today', '\\[NUMBER\\]': 'Five'}));
+
+async function fileModification () {
+    const sample = { "\\[WEBSITE_CONTACT_EMAIL\\]": "ade@gmail.com" }
+
+    try{
+        const file = await fsPromises.readFile(path.join(__dirname, '..', 'files', 't-c-template.txt'), 'utf-8')
+        console.log(file.allReplace(sample))
+    }catch(err){
+        console.error(err)
+    }
+}
+
+fileModification()
 
 module.exports = { userVerification }
