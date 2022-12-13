5cc6678317c78e09313722550b5b807969af7d55
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2022-12-13 20:36:03 +0100

    Proper linking of policy.html anchor tags

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,5 +8,32 @@ $('.open-btn').on('click', function(){
 $('.close-btn').on('click', function(){
     $('.sidebar').removeClass('active');
 });
+$('#website').click(function() {
+    if( $(this).is(':checked')) {
+    $("#div_website").removeClass('hidden');
+    $(this).addClass('show');
+}else{
+    $("#div_website").addClass('hidden');
+    $(this).removeClass('show');
+}
+});
+$('#app').click(function() {
+    if( $(this).is(':checked')) {
+    $("#div_app").removeClass('hidden');
+    $(this).addClass('show');
+}else{
+    $("#div_app").addClass('hidden');
+    $(this).removeClass('show');
+}
+});
+$('#facebook').click(function() {
+    if( $(this).is(':checked')) {
+    $("#div_facebook").removeClass('hidden');
+    $(this).addClass('show');
+}else{
+    $("#div_facebook").addClass('hidden');
+    $(this).removeClass('show');
+}
+});
 
 
