6b0d63aa494d3e82cf57cb7ebdc8fddec9efcbb7
ginohmk <michotall95@gmail.com>
2022-12-14 20:37:57 +0100

    feat: Add bottom nav bar

--- tc_gen_mobile/lib/pages/home_page.dart
+++ tc_gen_mobile/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:tc_gen_mobile/pages/component/bottom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +15,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello'),
-    );
+    return Scaffold(body: BottomBar());
   }
 }
