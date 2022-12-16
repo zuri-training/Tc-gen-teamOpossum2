2e5233b850a9cc7ea924df5054d4569c9cc4b75c
Dev-wonderful <adeonederful20@gmail.com>
2022-12-16 21:12:09 +0100

    Redirect after login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,7 @@ const login = async (req, res) => {
         const token = createToken(user._id)
 
         // Response
-        res.status(200).json({ user, token }).redirect('/')
+        res.status(200).redirect('/')
     } catch (err) {
         res.status(400).json({ error: err.message })
     }
