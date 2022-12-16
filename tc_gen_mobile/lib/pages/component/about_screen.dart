666040981a102585f7825029cc3ad7d2bbe493ca
ginohmk <michotall95@gmail.com>
2022-12-16 04:22:32 +0100

    feat : Add about page

--- tc_gen_mobile/lib/pages/component/about_screen.dart
+++ tc_gen_mobile/lib/pages/component/about_screen.dart
@@ -20,12 +20,52 @@ class _AboutScreenState extends State<AboutScreen> {
               padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
               child: Column(
                 children: [
-                  Text(
-                    'About Us',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.black87),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 4,
+                    child: Image.asset("assets/images/about_us.png",
+                        width: MediaQuery.of(context).size.width,
+                        height: 100,
+                        fit: BoxFit.contain),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    child: Text(
+                      'About Us',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 27,
+                          color: Colors.black87),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 15.0, left: 5, right: 5),
+                    child: Column(
+                      children: [
+                        Text(
+                          'We at TC Generator, provide customized privacy and TOS for web, Android & IOS app, Facebook App, Twitter developer account, Wordpress, Shopify and lots more.',
+                          style: TextStyle(fontSize: 16, height: 1.5),
+                          textAlign: TextAlign.justify,
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                          child: Text(
+                            'We are constantly looking for regulations around the world in order to creat the best and most up to date policies for your business.',
+                            style: TextStyle(fontSize: 16, height: 1.5),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: Text(
+                            'We allow you edit and cutomize manually your policies and also lets you download them on PDF, DOCX, TXT and HTML format.',
+                            style: TextStyle(fontSize: 16, height: 1.5),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
